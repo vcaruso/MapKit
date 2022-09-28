742834123306a6522a2d0a8ac31b3d3a07991aaa
Vincenzo Caruso <enzo@carusovincenzo.com>
2022-09-28 18:06:08 +0200

    Initial Commit

---
+++ MapKit/MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitApp.swift
+//  MapKit
+//
+//  Created by Vincenzo Caruso on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
